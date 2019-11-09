183576c039a1aa2b2c69954ee5891bff58d6756a
Felipe Gavilan <elmejor_887@hotmail.com>
2019-11-09 19:23:54 -0400

    Module 5

---
+++ Module5/Start/BlazorMovies/Client/Program.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.AspNetCore.Blazor.Hosting;
+
+namespace BlazorMovies.Client
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            CreateHostBuilder(args).Build().Run();
+        }
+
+        public static IWebAssemblyHostBuilder CreateHostBuilder(string[] args) =>
+            BlazorWebAssemblyHost.CreateDefaultBuilder()
+                .UseBlazorStartup<Startup>();
+    }
+
+}
