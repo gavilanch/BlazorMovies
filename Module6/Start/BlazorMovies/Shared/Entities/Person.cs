cd52c74f713e9726af79a41209f7b2f41ddc411c
Felipe Gavilan <elmejor_887@hotmail.com>
2019-11-18 09:59:46 -0400

    Agregado codigo modulo 6

---
+++ Module6/Start/BlazorMovies/Shared/Entities/Person.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Text;
+
+namespace BlazorMovies.Shared.Entities
+{
+    public class Person
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Biography { get; set; }
+        public string Picture { get; set; }
+        [Required]
+        public DateTime? DateOfBirth { get; set; }
+    }
+}
