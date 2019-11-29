3691ece46bcaacc8884009302739ec30c54af2ea
Felipe Gavilan <elmejor_887@hotmail.com>
2019-11-29 08:58:14 -0400

    Added Module 7 Code

---
+++ Module7/End/BlazorMovies/Server/ApplicationDbContext.cs
@@ -0,0 +1,35 @@
+﻿using BlazorMovies.Shared.Entities;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace BlazorMovies.Server
+{
+    public class ApplicationDbContext: IdentityDbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
+            :base(options)
+        {
+
+        }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<MoviesActors>().HasKey(x => new { x.MovieId, x.PersonId });
+            modelBuilder.Entity<MoviesGenres>().HasKey(x => new { x.MovieId, x.GenreId });
+
+            base.OnModelCreating(modelBuilder);
+        }
+
+
+        public DbSet<Genre> Genres { get; set; }
+        public DbSet<Movie> Movies { get; set; }
+        public DbSet<Person> People { get; set; }
+        public DbSet<MoviesActors> MoviesActors { get; set; }
+        public DbSet<MoviesGenres> MoviesGenres { get; set; }
+        public DbSet<MovieRating> MovieRatings { get; set; }
+    }
+}
