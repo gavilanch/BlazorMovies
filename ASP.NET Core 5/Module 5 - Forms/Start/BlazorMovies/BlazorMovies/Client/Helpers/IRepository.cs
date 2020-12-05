2a95a82bb20f00591ecc888c18b66b1f07365616
Felipe Gavilan <elmejor_887@hotmail.com>
2020-12-05 12:13:40 -0400

    Added module 5

---
+++ ASP.NET Core 5/Module 5 - Forms/Start/BlazorMovies/BlazorMovies/Client/Helpers/IRepository.cs
@@ -0,0 +1,13 @@
+﻿using BlazorMovies.Shared.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace BlazorMovies.Client.Helpers
+{
+    public interface IRepository
+    {
+        List<Movie> GetMovies();
+    }
+}
---
+++ ASP.NET Core 5/Module 5 - Forms/Start/BlazorMovies/BlazorMovies/Shared/Entities/Movie.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BlazorMovies.Shared.Entities
+{
+    public class Movie
+    {
+        public int Id { get; set; } = 1;
+        public string Title { get; set; }
+        public DateTime ReleaseDate { get; set; }
+        public string Poster { get; set; }
+        public string TitleBrief
+        {
+            get
+            {
+                if (string.IsNullOrEmpty(Title))
+                {
+                    return null;
+                }
+
+                if (Title.Length > 60)
+                {
+                    return Title.Substring(0, 60) + "...";
+                }
+                else
+                {
+                    return Title;
+                }
+            }
+        }
+    }
+}
