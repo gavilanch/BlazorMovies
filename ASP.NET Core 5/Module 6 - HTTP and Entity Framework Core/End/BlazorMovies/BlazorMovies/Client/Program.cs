4c0ac73e2f8338ee5bb03f0016a1424c031e8040
Felipe Gavilan <elmejor_887@hotmail.com>
2020-12-11 17:21:21 -0400

    Added module 6

---
+++ ASP.NET Core 5/Module 6 - HTTP and Entity Framework Core/End/BlazorMovies/BlazorMovies/Client/Program.cs
@@ -0,0 +1,38 @@
+using BlazorMovies.Client.Helpers;
+using BlazorMovies.Client.Repository;
+using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Generic;
+using System.Net.Http;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BlazorMovies.Client
+{
+    public class Program
+    {
+        public static async Task Main(string[] args)
+        {
+            var builder = WebAssemblyHostBuilder.CreateDefault(args);
+            builder.RootComponents.Add<App>("#app");
+
+            builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
+
+            ConfigureServices(builder.Services);
+
+            await builder.Build().RunAsync();
+        }
+
+        private static void ConfigureServices(IServiceCollection services)
+        {
+            services.AddTransient<IRepository, RepositoryInMemory>();
+            services.AddScoped<IHttpService, HttpService>();
+            services.AddScoped<IGenreRepository, GenreRepository>();
+            services.AddScoped<IPersonRepository, PersonRepository>();
+            services.AddScoped<IMoviesRepository, MoviesRepository>();
+        }
+    }
+}
---
+++ ASP.NET Core 5/Module 6 - HTTP and Entity Framework Core/End/BlazorMovies/BlazorMovies/Server/Helpers/InAppStorageService.cs
@@ -0,0 +1,64 @@
+﻿using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Http;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace BlazorMovies.Server.Helpers
+{
+    public class InAppStorageService : IFileStorageService
+    {
+        private readonly IWebHostEnvironment env;
+        private readonly IHttpContextAccessor httpContextAccessor;
+
+        public InAppStorageService(IWebHostEnvironment env,
+            IHttpContextAccessor httpContextAccessor
+            )
+        {
+            this.env = env;
+            this.httpContextAccessor = httpContextAccessor;
+        }
+
+        public Task DeleteFile(string fileRoute, string containerName)
+        {
+            var fileName = Path.GetFileName(fileRoute);
+            string fileDirectory = Path.Combine(env.WebRootPath, containerName, fileName);
+            if (File.Exists(fileDirectory))
+            {
+                File.Delete(fileDirectory);
+            }
+
+            return Task.FromResult(0);
+        }
+
+        public async Task<string> EditFile(byte[] content, string extension, string containerName, string fileRoute)
+        {
+            if (!string.IsNullOrEmpty(fileRoute))
+            {
+                await DeleteFile(fileRoute, containerName);
+            }
+
+            return await SaveFile(content, extension, containerName);
+        }
+
+        public async Task<string> SaveFile(byte[] content, string extension, string containerName)
+        {
+            var fileName = $"{Guid.NewGuid()}.{extension}";
+            string folder = Path.Combine(env.WebRootPath, containerName);
+
+            if (!Directory.Exists(folder))
+            {
+                Directory.CreateDirectory(folder);
+            }
+
+            string savingPath = Path.Combine(folder, fileName);
+            await File.WriteAllBytesAsync(savingPath, content);
+
+            var currentUrl = $"{httpContextAccessor.HttpContext.Request.Scheme}://{httpContextAccessor.HttpContext.Request.Host}";
+            var pathForDatabase = Path.Combine(currentUrl, containerName, fileName);
+            return pathForDatabase;
+        }
+    }
+}
