394a2b42903162beef3426c3f8fc500bbb426c99
Felipe Gavilan <elmejor_887@hotmail.com>
2020-12-18 21:56:42 -0400

    Added Rock, Paper and Scissors

---
+++ ASP.NET Core 5/Module 3 - Components/End/RockPaperAndScissors/RockPaperAndScissors/Program.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
+using System.Threading.Tasks;
+using Microsoft.Extensions.DependencyInjection;
+using System;
+using System.Net.Http;
+
+namespace RockPaperAndScissors
+{
+    public class Program
+    {
+        public static async Task Main(string[] args)
+        {
+            var builder = WebAssemblyHostBuilder.CreateDefault(args);
+            builder.RootComponents.Add<App>("#app");
+            builder.Services.AddSingleton(new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
+
+            await builder.Build().RunAsync();
+        }
+    }
+}
